9df76db2df00c9c67ad00860fa4d44a72fe33728
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2018-04-01 21:44:42 +0530

    Prettify

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
-const wrapAnsi = require('wrap-ansi');
-const cliBoxes = require('cli-boxes');
-
+const wrapAnsi = require("wrap-ansi");
+const cliBoxes = require("cli-boxes");
 
 const defaultOptions = {
   boxWidth: 30,
-  spikeDirection: 'right',
-  spikePosition: 10,
-}
+  spikeDirection: "right",
+  spikePosition: 10
+};
 
 const getBox = (message, options) => {
   options = Object.assign(defaultOptions, options);
@@ -19,25 +18,30 @@ const getBox = (message, options) => {
   if (message.length < boxWidth) boxWidth = message.length + 4;
   if (boxWidth < spikePosition) spikePosition = boxWidth / 2;
 
-
   const boxChars = cliBoxes.round;
-  const topBorder = boxChars.topLeft + boxChars.horizontal.repeat(boxWidth - 2) + boxChars.topRight;
-  const bottomBorder = boxChars.bottomLeft + boxChars.horizontal.repeat(boxWidth - 2) + boxChars.bottomRight;
+  const topBorder =
+    boxChars.topLeft +
+    boxChars.horizontal.repeat(boxWidth - 2) +
+    boxChars.topRight;
+  const bottomBorder =
+    boxChars.bottomLeft +
+    boxChars.horizontal.repeat(boxWidth - 2) +
+    boxChars.bottomRight;
 
-  let lines = wrapAnsi(message, boxWidth - 4, {hard: true}).split(/\n/);
+  let lines = wrapAnsi(message, boxWidth - 4, { hard: true }).split(/\n/);
 
   // Add vertical borders and paddings
   lines = lines.map(line => {
     const rightPaddingCount = boxWidth - 4 - line.length;
-    const rightPadding = ' '.repeat(rightPaddingCount);
-    return boxChars.vertical + ' ' + line + ' ' + rightPadding + boxChars.vertical;
+    const rightPadding = " ".repeat(rightPaddingCount);
+    return boxChars.vertical + " " + line + " " + rightPadding + boxChars.vertical;
   });
 
-  let result = topBorder + '\n' + lines.join('\n') + '\n' + bottomBorder;
+  let result = topBorder + "\n" + lines.join("\n") + "\n" + bottomBorder;
 
-  const spike = spikeDirection === 'right' ? '\\' : '/';
-  result += '\n' + ' '.repeat(spikePosition) + spike;
+  const spike = spikeDirection === "right" ? "\\" : "/";
+  result += "\n" + " ".repeat(spikePosition) + spike;
   return result;
-}
+};
 
-module.exports = { get: getBox }
+module.exports = { get: getBox };
